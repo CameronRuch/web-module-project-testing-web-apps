1f0085062845b479a143e8b4649b37d62d2c2982
CameronRuch <cameron.ruch@yahoo.com>
2022-03-11 05:42:54 -0600

    completed task 3 and 4

--- frontend/components/ContactForm.test.js
+++ frontend/components/ContactForm.test.js
@@ -20,10 +20,19 @@ test('renders the contact form header', () => {
 });
 
 test('renders ONE error message if user enters less then 5 characters into firstname.', async () => {
-
+    const firstName = screen.getByLabelText(/First Name*/i)
+    userEvent.type(firstName, "ab")
+    const errorMessages = await screen.findAllByTestId('error')
+    expect(errorMessages).toHaveLength(1)
 });
 
 test('renders THREE error messages if user enters no values into any fields.', async () => {
+    const submitBtn = screen.getByRole("button")
+    userEvent.click(submitBtn)
+    await waitFor(() => {
+        const errorMessages = screen.queryAllByTestId('error')
+        expect(errorMessages).toHaveLength(3)
+    })
 
 });
 
